6efb9c0626527aa7a0674e52c0048c168f78cb0d
Ricardo Satoshi Ikeima <ricardo.satoshi@yahoo.com.br>
2017-08-12 08:43:04 -0400

    Netbeans files

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Ricardo Satoshi Ikeima
+ *
+ * Created on August 12, 2017, 8:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
