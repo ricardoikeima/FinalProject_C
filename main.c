63093c869536344282beede46f79cc7ebc4d9271
Ricardo Satoshi Ikeima <ricardo.satoshi@yahoo.com.br>
2017-08-12 22:20:25 -0400

    New version

--- main.c
+++ main.c
@@ -30,7 +30,8 @@ typedef struct node{
 
 int getIndex(ContactPtr);
 ContactPtr createContact(char [], char [], int);
-
+ContactPtr searchContact (char [], char [], ContactPtr []);
+ContactPtr deleteContact(char [], char [], ContactPtr []);
 /*
  * 
  */
@@ -65,11 +66,13 @@ int main() {
             printf("Enter last name: ");
             scanf("%s", lastName);
             printf("Enter phone number: ");
-            scanf("%s", phoneNumber);
+            scanf("%d", &phoneNumber);
             
             ContactPtr contact = createContact(firstName, lastName, phoneNumber);
-                
-            contacts[getIndex(contact)] = contact;
+            
+            int index = getIndex(contact);
+            
+            contacts[index] = contact;
         } else if (select == 2){
             // TODO
         } else if (select == 3) {
@@ -83,8 +86,37 @@ int main() {
             ContactPtr contact = searchContact(firstName, lastName, contacts);
             
             if (contact == NULL){
-                printf("Contact not found!");
+                printf("Contact not found!\n");
+            } else {
+                printf("First name: %s\n", contact -> data.firstName);
+                printf("Last name: %s\n", contact -> data.lastName);
+                printf("Phone number: %d\n\n", contact -> data.phoneNumber);
             }
+        } else if (select == 4){
+            char firstName[50];
+            char lastName[50];
+            printf("Enter first name: ");
+            scanf("%s", firstName);
+            printf("Enter last name: ");
+            scanf("%s", lastName);
+            
+            ContactPtr contact = searchContact(firstName, lastName, contacts);
+            
+            if (contact == NULL){
+                printf("Contact not found!\n");
+            } else {
+                printf("The following contact:\n");
+                printf("First name: %s\n", contact -> data.firstName);
+                printf("Last name: %s\n", contact -> data.lastName);
+                printf("First name: %d\n", contact -> data.phoneNumber); 
+                printf("Was successfully deleted!\n");
+            }
+        } else if (select == 5){
+            //TO DO
+        } else if (select == 6){
+            //TO DO           
+        } else if (select == 7){
+            //TO DO           
         }
     }
 
@@ -123,13 +155,39 @@ ContactPtr createContact(char firstName[], char lastName[], int phoneNumber){
 // Search contact
 ContactPtr searchContact (char firstName[], char lastName[], ContactPtr array[]){
 
-    ContactPtr contact = array[getIndex(input)];
+    ContactPtr input = createContact(firstName, lastName, 0);
+    
+    int index = getIndex(input);
+    ContactPtr contact = array[index];
        
     while (contact != NULL){
-        if (strcmp(contact -> data.firstName, firstName) != 0 && strcmp(contact -> data.lastName, lastName)){
+        if (strcmp(contact -> data.firstName, firstName) == 0 && strcmp(contact -> data.lastName, lastName) == 0){
             return contact;
         } else {
             contact = contact -> next;
         }
     }
+    
+    return NULL;
+}
+
+// Delete contact
+ContactPtr deleteContact (char firstName[], char lastName[], ContactPtr array[]){
+
+    ContactPtr input = createContact(firstName, lastName, 0);
+    
+    int index = getIndex(input);
+    
+    ContactPtr contact = array[index];
+       
+    while (contact != NULL){
+        if (strcmp(contact -> data.firstName, firstName) == 0 && strcmp(contact -> data.lastName, lastName) == 0){
+            array[index] = NULL;
+            return contact;
+        } else {
+            contact = contact -> next;
+        }
+    }
+    
+    return NULL;
 }
