b1657b523b8cfcc4d62deaf47c4e8fe0a06b2d21
Ricardo Satoshi Ikeima <ricardo.satoshi@yahoo.com.br>
2017-08-14 06:37:10 -0400

    Sort fixed

--- main.c
+++ main.c
@@ -14,8 +14,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_CONTACTS 500
+#define FLUSH while (getchar() != '\n')
 
 typedef struct { 
     char firstName[50];
@@ -61,219 +63,228 @@ int main() {
         scanf("%d", &select);
         
         system("@cls||clear");
-        
-        if (select == 1){
-            char firstName[50];
-            char lastName[50];
-            int phoneNumber;
-            printf("Enter first name: ");
-            scanf("%s", firstName);
-            printf("Enter last name: ");
-            scanf("%s", lastName);
-            printf("Enter phone number: ");
-            scanf("%f", &phoneNumber);
-            
-            ContactPtr newContact = createContact(firstName, lastName, phoneNumber);
-            
-            int index = getIndex(firstName, lastName);
-            
-            addContact(index, newContact, contacts);
-            
-            system("@cls||clear");            
-            
-            printf("Contact successfully added!\n\n");
-            
-        } else if (select == 2){
-            char firstName[50];
-            char lastName[50];
-            printf("Enter first name: ");
-            scanf("%s", firstName);
-            printf("Enter last name: ");
-            scanf("%s", lastName);
-            
-            int index = getIndex(firstName, lastName);
-            
-            ContactPtr contact = contacts[index];
-            
-             if (contact == NULL){
-                printf("Contact not found!\n");       
-            } else {
-                printf("Current phone number: %.0f\n\n", contact -> data.phoneNumber);
-                printf("Enter new phone number: ");
-                scanf("%f", &contact -> data.phoneNumber);
-                
-                system("@cls||clear");
-                
-                printf("New phone number saved!\n\n");
-            }
-            
-        } else if (select == 3) {
-            char firstName[50];
-            char lastName[50];
-            printf("Enter first name: ");
-            scanf("%s", firstName);
-            printf("Enter last name: ");
-            scanf("%s", lastName);
-            
-            int index = getIndex(firstName, lastName);
-            
-            ContactPtr contact = contacts[index];
-            
-            while (contact != NULL){
-                if (strcmp(contact -> data.firstName, firstName) == 0 && strcmp(contact -> data.lastName, lastName) == 0){
-                    break;
+        if (isdigit(select + '0')){
+            if (select == 1){
+                char firstName[50];
+                char lastName[50];
+                int phoneNumber;
+                printf("Enter first name: ");
+                scanf("%s", firstName);
+                printf("Enter last name: ");
+                scanf("%s", lastName);
+                printf("Enter phone number: ");
+                scanf("%f", &phoneNumber);
+
+                ContactPtr newContact = createContact(firstName, lastName, phoneNumber);
+
+                int index = getIndex(firstName, lastName);
+
+                addContact(index, newContact, contacts);
+
+                system("@cls||clear");            
+
+                printf("Contact successfully added!\n\n");
+
+            } else if (select == 2){
+                char firstName[50];
+                char lastName[50];
+                printf("Enter first name: ");
+                scanf("%s", firstName);
+                printf("Enter last name: ");
+                scanf("%s", lastName);
+
+                int index = getIndex(firstName, lastName);
+
+                ContactPtr contact = contacts[index];
+
+                 if (contact == NULL){
+                    printf("Contact not found!\n");       
                 } else {
-                    contact = contact -> next;
+                    printf("Current phone number: %.0f\n\n", contact -> data.phoneNumber);
+                    printf("Enter new phone number: ");
+                    scanf("%f", &contact -> data.phoneNumber);
+
+                    system("@cls||clear");
+
+                    printf("New phone number saved!\n\n");
                 }
-            }
-            
-            if (contact == NULL){
-                printf("Contact not found!\n\n");
-            } else {
-                system("@cls||clear");
-                
-                printf("First name: %s\n", contact -> data.firstName);
-                printf("Last name: %s\n", contact -> data.lastName);
-                printf("Phone number: %.0f\n\n", contact -> data.phoneNumber);                                           
-            }
-            
-        } else if (select == 4){
-            char firstName[50];
-            char lastName[50];
-            printf("Enter first name: ");
-            scanf("%s", firstName);
-            printf("Enter last name: ");
-            scanf("%s", lastName);
-            
-            int index = getIndex(firstName, lastName);
-            
-            ContactPtr contact = contacts[index];
-            
-            if (contact == NULL){
-                printf("Contact not found!\n");
-            } else {
-                printf("The following contact:\n");
-                printf("First name: %s\n", contact -> data.firstName);
-                printf("Last name: %s\n", contact -> data.lastName);
-                printf("First name: %.0f\n", contact -> data.phoneNumber); 
-                
-                system("@cls||clear");
-                
-                printf("Was successfully deleted!\n\n");
-                
-                contacts[index] = NULL;
-            }
-        } else if (select == 5){
-            // Write file
-            FILE *out = fopen("contacts.txt", "w");
-            
-            for(int i = 0; i <= MAX_CONTACTS; i++){
-                if (contacts[i] != 0){
-                    fprintf(out, "%s ", contacts[i] -> data.firstName);
-                    fprintf(out, "%s ", contacts[i] -> data.lastName);
-                    fprintf(out, "%0.f\n", contacts[i] -> data.phoneNumber);
-                    
-                    ContactPtr nextContact = contacts[i] -> next;
-                    
-                    while (nextContact != NULL){
-                        fprintf(out, "%s ", nextContact -> data.firstName);
-                        fprintf(out, "%s ", nextContact -> data.lastName);
-                        fprintf(out, "%0.f\n", nextContact -> data.phoneNumber);
-                        
-                        nextContact = nextContact -> next;
+
+            } else if (select == 3) {
+                char firstName[50];
+                char lastName[50];
+                printf("Enter first name: ");
+                scanf("%s", firstName);
+                printf("Enter last name: ");
+                scanf("%s", lastName);
+
+                int index = getIndex(firstName, lastName);
+
+                ContactPtr contact = contacts[index];
+
+                while (contact != NULL){
+                    if (strcmp(contact -> data.firstName, firstName) == 0 && strcmp(contact -> data.lastName, lastName) == 0){
+                        break;
+                    } else {
+                        contact = contact -> next;
                     }
                 }
-            }
-            
-            fclose(out);
-            
-            system("@cls||clear");
-            
-            printf("File Saved!\n");
-            
-        } else if (select == 6){
-            ContactPtr temp = (ContactPtr) malloc(sizeof(Contact));
-            
-            // Clear contact list
-            for(int i = 1; i <= MAX_CONTACTS; i++){
-                if (contacts[i] != NULL){
-                    contacts[i] = NULL;
-                }
-            }
-            
-            // Read file
-            FILE *in = fopen("contacts.txt", "r");
-            
-            // Get name and grade until EOF
-            while (fscanf(in, "%s", temp -> data.firstName) != EOF){
 
-                fscanf(in, "%s", temp -> data.lastName);
-                fscanf(in, "%f", &temp -> data.phoneNumber);
-                temp -> next = NULL;
-                
-                int index = getIndex(temp -> data.firstName, temp -> data.lastName);
-                
-                addContact(index, temp, contacts);
-            }
-            
-            fclose(in);
-        } else if (select == 7){
-            sortContacts(contacts);
-        } else if (select == 8){
-            printf("Contact List\n");
-            int counter = 1;
-            for(int i = 1; i <= MAX_CONTACTS; i++){
-                if (contacts[i] != NULL){
-                    printf("Contact number: %d\n", counter++);
-                    printf("First Name: %s\n", contacts[i] -> data.firstName );
-                    printf("Last Name: %s\n", contacts[i] -> data.lastName );
-                    printf("Phone Number: %.0f\n\n", contacts[i] -> data.phoneNumber ); 
-                    
-                    ContactPtr nextContact = contacts[i] -> next;
-                    
-                    while (nextContact != NULL){
+                if (contact == NULL){
+                    printf("Contact not found!\n\n");
+                } else {
+                    system("@cls||clear");
+
+                    printf("First name: %s\n", contact -> data.firstName);
+                    printf("Last name: %s\n", contact -> data.lastName);
+                    printf("Phone number: %.0f\n\n", contact -> data.phoneNumber);                                           
+                }
+
+            } else if (select == 4){
+                char firstName[50];
+                char lastName[50];
+                printf("Enter first name: ");
+                scanf("%s", firstName);
+                printf("Enter last name: ");
+                scanf("%s", lastName);
+
+                int index = getIndex(firstName, lastName);
+
+                ContactPtr contact = contacts[index];
+
+                if (contact == NULL){
+                    printf("Contact not found!\n");
+                } else {
+                    printf("The following contact:\n");
+                    printf("First name: %s\n", contact -> data.firstName);
+                    printf("Last name: %s\n", contact -> data.lastName);
+                    printf("First name: %.0f\n", contact -> data.phoneNumber); 
+
+                    system("@cls||clear");
+
+                    printf("Was successfully deleted!\n\n");
+
+                    contacts[index] = NULL;
+                }
+            } else if (select == 5){
+                // Write file
+                FILE *out = fopen("contacts.txt", "w");
+
+                for(int i = 0; i <= MAX_CONTACTS; i++){
+                    if (contacts[i] != 0){
+                        fprintf(out, "%s ", contacts[i] -> data.firstName);
+                        fprintf(out, "%s ", contacts[i] -> data.lastName);
+                        fprintf(out, "%0.f\n", contacts[i] -> data.phoneNumber);
+
+                        ContactPtr nextContact = contacts[i] -> next;
+
+                        while (nextContact != NULL){
+                            fprintf(out, "%s ", nextContact -> data.firstName);
+                            fprintf(out, "%s ", nextContact -> data.lastName);
+                            fprintf(out, "%0.f\n", nextContact -> data.phoneNumber);
+
+                            nextContact = nextContact -> next;
+                        }
+                    }
+                }
+
+                fclose(out);
+
+                system("@cls||clear");
+
+                printf("File Saved!\n");
+
+            } else if (select == 6){
+                ContactPtr temp = (ContactPtr) malloc(sizeof(Contact));
+
+                // Clear contact list
+                for(int i = 1; i <= MAX_CONTACTS; i++){
+                    if (contacts[i] != NULL){
+                        contacts[i] = NULL;
+                    }
+                }
+
+                // Read file
+                FILE *in = fopen("contacts.txt", "r");
+
+                // Get name and grade until EOF
+                while (fscanf(in, "%s", temp -> data.firstName) != EOF){
+
+                    fscanf(in, "%s", temp -> data.lastName);
+                    fscanf(in, "%f", &temp -> data.phoneNumber);
+                    temp -> next = NULL;
+
+                    int index = getIndex(temp -> data.firstName, temp -> data.lastName);
+
+                    addContact(index, temp, contacts);
+                }
+
+                fclose(in);
+            } else if (select == 7){
+                sortContacts(contacts);
+            } else if (select == 8){
+                printf("Contact List\n");
+                int counter = 1;
+                for(int i = 1; i <= MAX_CONTACTS; i++){
+                    if (contacts[i] != NULL){
                         printf("Contact number: %d\n", counter++);
-                        printf("First Name: %s\n", nextContact -> data.firstName );
-                        printf("Last Name: %s\n", nextContact -> data.lastName );
-                        printf("Phone Number: %.0f\n\n", nextContact -> data.phoneNumber );
-                        
-                        nextContact = nextContact -> next;
+                        printf("First Name: %s\n", contacts[i] -> data.firstName );
+                        printf("Last Name: %s\n", contacts[i] -> data.lastName );
+                        printf("Phone Number: %.0f\n\n", contacts[i] -> data.phoneNumber ); 
+
+                        ContactPtr nextContact = contacts[i] -> next;
+
+                        while (nextContact != NULL){
+                            printf("Contact number: %d\n", counter++);
+                            printf("First Name: %s\n", nextContact -> data.firstName );
+                            printf("Last Name: %s\n", nextContact -> data.lastName );
+                            printf("Phone Number: %.0f\n\n", nextContact -> data.phoneNumber );
+
+                            nextContact = nextContact -> next;
+                        }
                     }
                 }
-            }
-        } else if (select == 9){
-            ContactPtr temp = (ContactPtr) malloc(sizeof(Contact));
-            
-            // Clear contact list
-            for(int i = 1; i <= MAX_CONTACTS; i++){
-                if (contacts[i] != NULL){
-                    contacts[i] = NULL;
-                }
-            }
-            
-            // Read file
-            FILE *in = fopen("sample.txt", "r");
-            
-            // Get name and grade until EOF
-            while (fscanf(in, "%s", temp -> data.firstName) != EOF){
+            } else if (select == 9){
+                ContactPtr temp = (ContactPtr) malloc(sizeof(Contact));
 
-                fscanf(in, "%s", temp -> data.lastName);
-                fscanf(in, "%f", &temp -> data.phoneNumber);
-                temp -> next = NULL;
-                
-                int index = getIndex(temp -> data.firstName, temp -> data.lastName);
-                
-                addContact(index, temp, contacts);
+                // Clear contact list
+                for(int i = 1; i <= MAX_CONTACTS; i++){
+                    if (contacts[i] != NULL){
+                        contacts[i] = NULL;
+                    }
+                }
+
+                // Read file
+                FILE *in = fopen("sample.txt", "r");
+
+                // Get name and grade until EOF
+                while (fscanf(in, "%s", temp -> data.firstName) != EOF){
+
+                    fscanf(in, "%s", temp -> data.lastName);
+                    fscanf(in, "%f", &temp -> data.phoneNumber);
+                    temp -> next = NULL;
+
+                    int index = getIndex(temp -> data.firstName, temp -> data.lastName);
+
+                    addContact(index, temp, contacts);
+                }
+
+                fclose(in);
+            } else if (select == 0){ // Exit program and message
+                    printf("Thank you!\n");
+                    printf("This program was developed by\n");
+                    printf("Ricardo Satoshi Ikeima\n");
+                    printf("Rongfu Li\n"); 
+                    printf("PROG20799 Data Structures & Algorithm Development - C\n"); 
+                    printf("Final Project - 14 - AUG - 2017\n");
+            } else {
+                printf("Invalid Option!\n");
+                FLUSH;
+                select = -1;
             }
-            
-            fclose(in);
-        } else if (select == 0){ // Exit program and message
-                printf("Thank you!\n");
-                printf("This program was developed by\n");
-                printf("Ricardo Satoshi Ikeima\n");
-                printf("Rongfu Li\n"); 
-                printf("PROG20799 Data Structures & Algorithm Development - C\n"); 
-                printf("Final Project - 14 - AUG - 2017\n");
+        } else {
+            printf("Invalid Option!\n");
+            FLUSH;
+            select = -1;
         }
     }
     return (0);
